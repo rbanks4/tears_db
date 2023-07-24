851ad2ffde72fd55000a7baddb53bd75e0dddfa5
Reggie Jr <p3anutbj7@gmail.com>
2023-07-23 21:25:28 -0400

    app will now remember nav location on rotate

---
+++ app/src/main/java/com/gaming/android/tearsdatabase/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.gaming.android.tearsdatabase
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+const val SELECTED_NAV_ITEM = "selected"
+class MainViewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
+    var navItem: String?
+        get() = savedStateHandle.get<String>(SELECTED_NAV_ITEM)
+        set(value) = savedStateHandle.set(SELECTED_NAV_ITEM, value)
+}
